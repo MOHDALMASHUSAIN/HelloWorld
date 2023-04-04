6a9ea9adcaf0df81a9c74f4d9e856827e3fdbe85
MOHD ALMAS HUSAIN <iconalmas@gmail.com>
2023-04-05 00:37:27 +0530

    I have changed;lm;ltfs

---
+++ src/kevin.java
@@ -0,0 +1,15 @@
+public class kevin {
+    public static int  max(int a, int b){
+       // System.out.println(a+b);
+        int c = a+b;
+        System.out.println(c);
+        return c;
+    }
+    public static void main(String[] args){
+
+   max(3,6);
+        System.out.println();
+
+
+    }
+}
